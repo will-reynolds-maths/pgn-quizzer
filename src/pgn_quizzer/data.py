a6cf2d9c012c7b755e34bd522c9a5f08ef3b9fe1
William Reynolds <wrn.reynolds@outlook.com>
2026-01-14 23:13:20 +0000

    Formatting and readability

--- src/pgn_quizzer/data.py
+++ src/pgn_quizzer/data.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from json import dumps, load
 from random import sample
 from chess import Board
+
 from pgn_quizzer.model import Question
 
 # pgn input -> storeable JSON -> "data" e.g. strs, lists, dicts -> questions (dataclass objs from model.py)
@@ -15,6 +16,7 @@ from pgn_quizzer.model import Question
 class ChessGame:
     title: str
     fen_assets: dict[str, list[str]] = field(default_factory=dict)
+
 
 class ChessGameConstructor:
     """
@@ -43,9 +45,6 @@ class ChessGameConstructor:
 
     def _get_fens(self, subpgn: str) -> dict[str, list[str]]:
         ...
-
-    def _construct_chessgame_object(self, title: str, asset_fens: dict[str, list[str]]):
-        return ChessGame(title, asset_fens)
     
     def construct(self):
         list_subpgns = self._separate_games_within_pgn()
@@ -59,7 +58,10 @@ class ChessGameConstructor:
             subpgn = self._strip_metadata(subpgn)
             subpgn_asset_fens = self._get_fens(subpgn)
             
-            game = self._construct_chessgame_object(subpgn_title, subpgn_asset_fens)
+            game = ChessGame(
+                title=subpgn_title, 
+                fen_assets=subpgn_asset_fens
+                )
             self.chessgames.append(game)
 
 
@@ -70,20 +72,26 @@ def sample_with_avoidance(lst: list[str], k: int, bad_val: str):
             break
     return sample_attempt
 
+
 # TODO: the following function is delicately implemented and needs to be tested well
 def generate_question_data(game: ChessGame, constructor: ChessGameConstructor, nb_options: int):
     """
     TODO: write a docstring
     """
     
-    question_text = {"desc1": "a",
-                     "desc2": "b",
-                     "desc3": "c",}
+    question_text = {
+        "desc1": "a",
+        "desc2": "b",
+        "desc3": "c",
+        }
     
-    question_data = {"text": "",
-                     "right_answer": game.title, # N.B.
-                     "wrong_answers": [],
-                     "assets": []}
+    question_data = {
+        "text": "",
+        "right_answer": game.title, # N.B.
+        "wrong_answers": [],
+        "assets": []
+        }
+    
     question_list = []
 
     for desc in game.fen_assets.keys():
@@ -97,6 +105,7 @@ def generate_question_data(game: ChessGame, constructor: ChessGameConstructor, n
             question_list.append(question_data)
     
     return question_list
+
 
 # I guess at this stage I'm including this function just in case
 # TODO: decide what you want to do with this function
@@ -113,6 +122,7 @@ def pgn_to_json(path: str, nb_options=3) -> str:
         json_array_list += question_data_list
     
     return dumps(json_array_list)
+
 
 #-------------------------
 # Quiz Data Processing
@@ -134,14 +144,16 @@ def pgn_loader(path: Path, nb_options=3) -> list[dict]:
     
     return question_data
 
+
 def json_loader(path: Path) -> list[dict]:
     '''Validated at runtime.'''
     with open(path, mode='r') as json:
         question_data = load(json)
     return question_data
 
+
 def is_valid_data(q: dict) -> bool:
-    # the data must be of the correct types
+    # the data values must be of the correct types
     if not isinstance(q["text"], str):
         return False
     elif not isinstance(q["right_answer"], str):
@@ -165,6 +177,7 @@ def is_valid_data(q: dict) -> bool:
 
     return valid
 
+
 def parse_data(question_data: list[dict]) -> list[Question]:
     '''Generates list of Question objects from question data; invalid data
     is ignored (skipped over).'''
@@ -176,6 +189,7 @@ def parse_data(question_data: list[dict]) -> list[Question]:
                      assets        = q["assets"])
             for q in valid_data
             ]
+
 
 #-------------------------
 # Create Question Bank
